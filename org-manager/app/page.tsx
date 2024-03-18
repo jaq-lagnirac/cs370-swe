af708c470f463829a08826438d5105cc9b949a6d
Akansha Negi <akanshanegi@gmail.com>
2024-03-18 14:51:39 -0500

    table component + bootstrap

--- org-manager/app/page.tsx
+++ org-manager/app/page.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import RootLayout from './layout';
+import Table from './components/table';
 
 export default function Roster() {
+  const columns = ["Name", "Email", "Banner ID"];
+  const members = [
+    {name: "Andrew Ruff", email: "aruff@gmail.com", id: 111111111},
+    {name: "Julian Williams", email: "jwilliams@gmail.com", id: 22222222},
+    {name: "Justin Caringal", email: "jcaringal@gmail.com", id: 333333333},
+  ];
   return (
-    <div>
+    <>
       <h1>Roster</h1>
-      <p>This is the Roster page.</p>
-    </div>
+      <Table columns={columns} members={members}/>
+    </>
   );
 }
