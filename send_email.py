bab288f50274145c26a6545a38b1f307769c951b
Justin Caringal <jacfriday10th@gmail.com>
2024-03-21 19:28:37 -0500

    Basic driver function set up
    -Future direction: to make auth more consistent

--- send_email.py
+++ send_email.py
@@ -1,53 +1,134 @@
-import os
-import smtplib
+#!/usr/bin/python3
+# Justin Caringal
+# A function which organizes and implements Gmail API calls to send emails
 
+# Email Libraries
 import base64
 from email.mime.text import MIMEText
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from requests import HTTPError
 
-# loads in private .env variables
-from dotenv import load_dotenv
-load_dotenv()
-EMAIL_ADDRESS = os.getenv('EMAIL_ADDRESS')
-EMAIL_PASSWORD = os.getenv('EMAIL_PASSWORD')
+# Multithreading Libraries
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import multiprocessing
 
+# Constants
+MAX_ATTEMPTS = 10
 DESCRIPTION = """Hello!
 Welcome to send_email.py"""
 
-SCOPES = [
-    "https://www.googleapis.com/auth/gmail.send"
-]
 
-flow = InstalledAppFlow.from_client_secrets_file(
-    'credentials.json', SCOPES
-)
+# Functions
 
-credentials = flow.run_local_server(port = 0)
+def send_email(service, recipient, subject, body):
+    """ Sends a single email
+    
+    A function which formats and encodes a message with the required
+    email information and uses the Google API to send the email
 
-service = build('gmail', 'v1', credentials = credentials)
+    Args:
+        service (api): The Gmail flow used to access the API
+        recipient (str): A string containing the email address that will
+            receive the email
+        subject (str): A string containing the subject header for the email
+        body (str): A string containing the raw body text for the email
 
-subject = 'this is a test subject for cs370'
-body = 'this is a test body for cs370'
-sender = EMAIL_ADDRESS
-recipients = [
-    'jac5566@truman.edu',
-    'an2713@truman.edu',
-    'jaw6642@truman.edu',
-    'abr8115@truman.edu'
+    Returns:
+        Returns confirmation (unusable by program)
+    """
+    # sets up email with required parts
+    message  = MIMEText(body)
+    message['to'] = recipient
+    message['subject'] = subject
+
+    # encodes message in base64
+    create_message = {
+        'raw' : base64.urlsafe_b64encode(message.as_bytes()).decode()
+    }
+
+    try:
+        message = (
+            service
+            .users()
+            .messages()
+            .send(userId='me', body=create_message)
+            .execute()
+            )
+        print(f'Sent message to {recipient}')
+    except HTTPError:
+        print(f'An HTTP error occured with {recipient}')
+        message = None
+    except Exception as error:
+        print(f'An unknown error occured with {recipient}\n{error}')
+        message = None
+
+    return message
+
+
+def sends_emails_to_list(service, recipients, subject, body):
+    """ Sends email to list of recipients
+
+     A function which takes a list of email addresses and sends
+     identical emails to each of the recipients. Does not use bcc
+
+     Args:
+        service (api): The Gmail flow used to access the API
+        recipients (list): A list of email address strings
+        subject (str): A string containing the subject header for the email
+        body (str): A string containing the raw body text for the email
+
+    Returns:
+        None
+    """
+    # processes tasks using ThreadPoolExecutor
+    errors = []
+    with ThreadPoolExecutor(max_workers = multiprocessing.cpu_count()) as executor:
+            for recipient in recipients:
+                try:
+                    executor.submit(send_email, service, recipient, subject, body)
+                except:
+                    errors.append(recipient)
+
+
+def send_email_driver(recipients, subject, body):
+    """ Driver function to handle OAuth
+
+    A function which handles the OAuth into the Google API for Gmail
+    and sends the emails
+    """
+    # sets scopes for use with Google API
+    SCOPES = [
+        "https://www.googleapis.com/auth/gmail.send"
     ]
+    # extracts client secrets for use with scope
+    flow = InstalledAppFlow.from_client_secrets_file(
+        'credentials.json', SCOPES
+    )
+    # runs local server to validate credentials
+    credentials = flow.run_local_server(port = 0)
+    # builds Gmail call with attached credentials
+    service = build('gmail', 'v1', credentials = credentials)
 
-message  = MIMEText(body)
-message['to'] = 'jac5566@truman.edu'
-message['subject'] = subject
-create_message = {
-    'raw' : base64.urlsafe_b64encode(message.as_bytes()).decode()
-}
+    # keeps attempting to send emails until stopped
+    successful_auth = False
+    for attempt in range(1, MAX_ATTEMPTS + 1):
+        try:
+            sends_emails_to_list(service, recipients, subject, body)
+            successful_auth = True
+            print('Successful authentication.')
+            break
+        except:
+            print(f'Attempt {attempt} failed. Error in authenticating into Google API.')
 
-try:
-    message = (service.users().messages().send(userId='me', body=create_message).execute())
-    print(f'Sent message to {message} Message ID: {message["id"]}')
-except HTTPError as error:
-    print(f'An error occured: {error}')
-    message = None
+    if not successful_auth:
+        print('Authentication into Google API unsuccessful.')
+
+if __name__ == '__main__':
+    recipients = ['jac5566@truman.edu',
+                  'jaw6642@truman.edu',
+                  'abr8115@truman.edu',
+                  'an2713@truman.edu']
+    subject = 'this is a test subject for cs370'
+    body = 'this is a test body for cs370'
+    send_email_driver(recipients, subject, body)
