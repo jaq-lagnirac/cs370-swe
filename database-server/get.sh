6a3a025305e7e03517b67ddf712aaa612ddfb547
Julian Williams <julian@wazworld.net>
2024-03-21 21:23:14 -0500

    Finished test server. Began working on the real deal

--- database-server/get.sh
+++ database-server/get.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-curl -i -X GET http://127.0.0.1:8080/
+if [[ -z $1 ]]
+then
+	echo "Missing parameter"
+	exit 1
+fi
+
+curl -i -X GET http://127.0.0.1:8080/api/$1
 echo ""
--- database-server/post.sh
+++ database-server/post.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-if [[ -z "$1" ]]; then
-	echo "Missing parameter"
+if [[ -z $1 ]]; then
+	echo "Missing name"
 	exit 1
 fi
 
-curl -i -X POST -H "Content-Type: application/json" -d "{\"value\": $1}" http://127.0.0.1:8080/
+if [[ -z $2 ]]; then
+	echo "Missing JSON"
+	exit 2
+fi
+
+curl -i -X POST -H "Content-Type: application/json" -d $2 http://127.0.0.1:8080/api/$1
 echo ""
