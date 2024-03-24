65d7962f4271de00ed4e96432e065c9707052acf
Julian Williams <julian@wazworld.net>
2024-03-24 14:53:46 -0500

    Finished roster-server.py with members table. Wrote up initial api spec

--- database-server/put.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ -z "$1" ]]; then
-	echo "Missing parameter"
-	exit 1
-fi
-
-curl -i -X PUT -H "Content-Type: application/json" -d "{\"value\": $1}" http://127.0.0.1:8080/
-echo ""
