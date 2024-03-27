689a73834c52bfb3eb0b0c6ff4cd8aec41280df5
Akansha Negi <akanshanegi@gmail.com>
2024-03-27 14:41:12 -0500

    showQR code button works

--- org-manager/app/attendance/page.tsx
+++ org-manager/app/attendance/page.tsx
@@ -1,20 +1,32 @@
+'use client';
 import React from 'react';
 import ImgDl from '../components/img-dl'
 import Table from '../components/table';
+import { useState } from 'react';
 import RootLayout from '../layout';
 
 export default function Attendance() {
   const columns = ["Date", "Time", "Attendees"];
   const members = [
-    {Date: "1970-01-01", Time: "10:00-12:00", Attendees: 111111111}
+    {Date: "1970-01-01", Time: "10:00-12:00", Attendees: 2}
   ];
+  const [showQR, setShowQR] = useState(false);
+
+  const showQRCOde = () => {
+    setShowQR(!showQR);
+  }
+
   return (
     <div>
       <h1>Attendance</h1>
-      <button className="attendance-button">Create Session</button>
-      <p>Here is a test QR code download.</p>
-      <ImgDl></ImgDl>
-      <Table columns={columns} members={members}/>
+      <button className="attendance-button" onClick={showQRCOde}>Create Session</button>
+      {showQR ? (
+        <>
+          <p>Here is a test QR code download.</p>
+          <ImgDl></ImgDl>
+          <Table columns={columns} members={members} />
+        </>
+      ) : ( <></> )}
     </div>
   );
 }
