ed5bf69e7eb9149207faf5b0aa5714cedf95df56
Akansha Negi <akanshanegi@gmail.com>
2024-04-07 15:06:44 -0500

    pushing this out early to push out SVG icons

--- org-manager/app/page.tsx
+++ org-manager/app/page.tsx
@@ -9,7 +9,7 @@ export default function Roster() {
     {"Name": "Julian Williams", "Email": "jww1111@truman.edu", "Banner ID": 103422344, "Role": "member"},
     {"Name": "Justin Caringal", "Email": "jac5566@truman.edu", "Banner ID": 1011113456, "Role": "president"},
     {"Name": "Akansha Negi", "Email": "an2713@truman.edu", "Banner ID": 1011234567, "Role": "exec"},
-    {"Name": "Halma Ruthie", "Email": "ruthie@truman.edu", "Banner ID": 105464445, "Role": "member"},
+    {"Name": "Ruthie Halma", "Email": "ruthie@truman.edu", "Banner ID": 105464445, "Role": "member"},
     {"Name": "Kafi Rahman", "Email": "kir2311@truman.edu", "Banner ID": 102042342, "Role": "member"},
     {"Name": "Ting Cao", "Email": "tac3912@truman.edu", "Banner ID": 102342332, "Role": "member"},
   ];
