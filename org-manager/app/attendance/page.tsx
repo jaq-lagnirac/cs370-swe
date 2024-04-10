b4889e86acdb296bcf262280e0380b06aa3a3368
Andrew Ruff <noreply@andrew.ruff>
2024-04-10 17:05:55 -0500

    Expanded attendance page

--- org-manager/app/attendance/page.tsx
+++ org-manager/app/attendance/page.tsx
@@ -1,7 +1,8 @@
 'use client';
 import React from 'react';
-import ImgDl from '../components/img-dl'
+import ImgDl from '../components/img-dl';
 import Table from '../components/table';
+import CopyText from '../components/copy-text';
 import { useState } from 'react';
 import RootLayout from '../layout';
 
@@ -15,6 +16,10 @@ export default function Attendance() {
   const showQRCOde = () => {
     setShowQR(!showQR);
   }
+  const createEvent = () => {
+    console.log("When we connect to the backend, this function will create a new event.");
+    // Also it will probably need to refresh the table so the new event shows up
+  }
 
   return (
     <div>
@@ -23,13 +28,16 @@ export default function Attendance() {
       {showQR ? (
         <>
           <h2 className="mb-2">Here is a test QR code download.</h2>
-          <div className="mb-5">
+          <CopyText/>
+          <div id="qrcode" className="mb-2">
             <ImgDl/>
           </div>
+          <button className="attendance-button mb-4" onClick={createEvent}>Confirm Creation</button>
         </>
       ) : ( <></> )}
       <div className="mb-3">
         <Table columns={columns} tableData={tableData} />
+
       </div>
     </div>
   );
--- org-manager/app/components/img-dl.tsx
+++ org-manager/app/components/img-dl.tsx
@@ -1,15 +1,21 @@
 import Image from 'next/image'
+import Script from 'next/script'
 import Link from 'next/link'
+import { renderSVG } from 'scannable/qr';
+import React from 'react';
 // An image that can be clicked to download, with a download symbol on hover
 
 function ImgDl() {
   // Temporary fiat definition, replace with an argument
-  let image_url = "/test-qr.png"; 
+  const testLink = "https://ruff.computer";
+  const qrCode = renderSVG(testLink);
+  const qrBlob = new Blob([qrCode]);
+  const qrURL = window.URL.createObjectURL(qrBlob);
   return (
   <>
-    <a href={image_url} download>
-        <Image src={image_url} alt="A QR code" width="256" height="256" />
-      </a>
+    <a href={qrURL} download="qrcode.svg">
+      <img src={`data:image/svg+xml;utf8,${encodeURIComponent(qrCode)}`} />
+    </a>
   </>
   );
 };
